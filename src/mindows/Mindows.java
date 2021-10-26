857e435683777e3043f0832563b880c2b603d37a
Goobrr <ggoobbrr@gmail.com>
2021-10-26 11:12:20 +0700

    Mobile Warning

--- src/mindows/Mindows.java
+++ src/mindows/Mindows.java
@@ -4,13 +4,20 @@ import arc.*;
 import mindows.ui.*;
 import mindustry.*;
 import mindustry.game.EventType.*;
+import mindustry.gen.Icon;
 import mindustry.mod.*;
 
 public class Mindows extends Mod{
     public Mindows(){
-        Events.on(ClientLoadEvent.class, h -> {
+        Events.on(ContentInitEvent.class, h -> {
             MindowsTex.init();
+        });
+
+        Events.on(ClientLoadEvent.class, h -> {
             new WindowFragment().build(Vars.ui.hudGroup);
+            if(Vars.mobile){
+                Vars.ui.showOkText("WARNING", "Mindows is untested on mobile and may break or show unexpected behavior, please report an issue if you do see one.\nThank you for installing Mindows!", () -> {});
+            };
         });
     }
 
