8dd6767e80cd0589f8a5b258a5361b10f3c82834
KayAyeAre <56635816+KayWasTaken@users.noreply.github.com>
2021-11-03 11:17:18 +0700

    Create ScriptsHandler.java

---
+++ src/mindows/scripts/ScriptsHandler.java
@@ -0,0 +1,17 @@
+package mindows;
+
+import arc.files.Fi;
+import mindustry.Vars;
+
+import static mindustry.Vars.dataDirectory;
+
+public class ScriptsHandler {
+    public static Fi scriptDirectory = dataDirectory.child("scripts/");
+    public static Fi[] scripts;
+    public static void load() {
+        scripts = scriptDirectory.list();
+        for (Fi script : scripts) {
+            Vars.mods.getScripts().runConsole(script.readString());
+        }
+    }
+}
